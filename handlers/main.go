6c327fddb3586f1f3a80e5771d50efd24cb8f660
attendee <attendees@atomicmail.io>
2025-11-23 03:06:34 +0300

    add: handlers

---
+++ handlers/main.go
@@ -0,0 +1,77 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/attendeee/url-shortener/storage/db"
+	"github.com/attendeee/url-shortener/storage/lite"
+	"github.com/attendeee/url-shortener/utils"
+	"github.com/gorilla/mux"
+)
+
+var All *mux.Router
+
+func Init() {
+	All = mux.NewRouter()
+	http.Handle("/", utils.CorsMiddleware(All))
+
+	All.HandleFunc("/url", func(w http.ResponseWriter, r *http.Request) {
+		urls, err := lite.Query.GetAll(context.Background())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln("Get all url error: ", err)
+		}
+
+		urlsResponse, err := json.Marshal(urls)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln("Get all url error: ", err)
+		}
+
+		w.WriteHeader(http.StatusFound)
+		w.Write([]byte(urlsResponse))
+
+	}).Methods("GET")
+
+	All.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		var url db.Url
+
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&url)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Fatalln("Create url error: ", err)
+		}
+
+		err = lite.Query.CreateShorthand(
+			context.Background(),
+			db.CreateShorthandParams{Longhand: url.Longhand, Shorthand: url.Shorthand},
+		)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatalln("Create url error: ", err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(url)
+
+	}).Methods("POST")
+
+	All.HandleFunc("/{url}", func(w http.ResponseWriter, r *http.Request) {
+		urlParam := mux.Vars(r)["url"]
+
+		url, err := lite.Query.GetByShorthand(context.Background(), urlParam)
+		if err != nil {
+			log.Println("Redirect url error: ", err)
+			http.Redirect(w, r, "/url", http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, url.Longhand, http.StatusFound)
+
+	}).Methods("GET")
+}
